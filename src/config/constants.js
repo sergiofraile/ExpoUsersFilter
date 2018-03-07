74ae2ee2c9cf3538dec331e4f241390ca9db9d64
Sergio Fraile <s.frailec@gmail.com>
2018-03-07 23:13:56 +0000

    Quick update max distance

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const EARTH_RADIUS = 6371009;
 export const MIN_SLIDER_DISTANCE = 0;
-export const MAX_SLIDER_DISTANCE = 700;
+export const MAX_SLIDER_DISTANCE = 250;
 export const INITIAL_DISTANCE = 100;
 export const OFFICE_LOCATION = {
   latitude: 53.339428,
